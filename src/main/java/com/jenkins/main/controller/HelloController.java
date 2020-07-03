2de15e7ee2878884d260b7f8a08b69367ccc47d9
lwl9710 <766649508@qq.com>
2020-07-03 19:00:59 +0800

    jenkins测试

---
+++ src/main/java/com/jenkins/main/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.jenkins.main.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Author: BlueSky
+ * Date: 2020-07-03 18:58
+ * Description: 问候语
+ */
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello jenkins";
+    }
+}

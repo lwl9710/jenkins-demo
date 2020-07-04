38fdaba1e933758c9499058665e8a19bdb4b2952
lwl9710 <766649508@qq.com>
2020-07-04 18:44:33 +0800

    jenkins测试

--- src/main/java/com/jenkins/main/JenkinsDemoApplication.java
+++ src/main/java/com/jenkins/main/JenkinsDemoApplication.java
@@ -2,9 +2,16 @@ package com.jenkins.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsDemoApplication {
+public class JenkinsDemoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(JenkinsDemoApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsDemoApplication.class, args);
